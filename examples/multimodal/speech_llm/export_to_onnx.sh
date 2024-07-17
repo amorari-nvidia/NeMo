7cd6e670a2ebeb2d9faf8f0e85f0299a12879e5d
Alessandro Morari <amorari@nvidia.com>
2024-07-17 12:18:52 +0000

    changed script to search for checkpoint in the current folder

--- examples/multimodal/speech_llm/export_to_onnx.sh
+++ examples/multimodal/speech_llm/export_to_onnx.sh
@@ -2,7 +2,7 @@
 #Instructions from: 
 # https://catalog.ngc.nvidia.com/orgs/nvidia/teams/nemo/models/speechllm_fc_llama2_7b
 set -x
-MEGATRON_CKPT=/workspace_host/salm/speechllm_fc_llama2_7b.nemo
+MEGATRON_CKPT=./speechllm_fc_llama2_7b.nemo
 CUDA_VISIBLE_DEVICES=0 python export_to_onnx.py \
 	model.restore_from_path=$MEGATRON_CKPT \
 	model.export_to_path=speechllm_fc_llama2_7b
